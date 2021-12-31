086411ef489e3b1b6f18e3bee5f645009cb448df
Brendan Armani <brendanarmani@gmail.com>
2021-12-30 23:31:03 -0500

    styling for meal blocks

--- components/MealListItem.js
+++ components/MealListItem.js
@@ -2,13 +2,28 @@ import React from "react";
 import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 
 const styles = StyleSheet.create({
-    mealContainer: {
-        marginVertical: 20,
-        paddingVertical: 15,
+    mealItem: {
+        flex: 1,
+        margin: 15,
+        height: 150,
         borderRadius: 10,
-        width: '100%',
-        backgroundColor: '#ccc',
-        textAlign: 'center'
+        elevation: 5
+    },
+    mealContainer: {
+        flex: 1,
+        backgroundColor: '#c3d',
+        borderRadius: 10,
+        shadowColor: 'black',
+        shadowOpacity: 0.26,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 10,
+        padding: 15,
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end'
+    },
+    mealTitle: {
+        fontSize: 22,
+        textAlign: 'right'
     }
 });
 
@@ -21,11 +36,13 @@ const MealListItem = (props) => {
     };
 
     return (
-        <TouchableOpacity onPress={handleNavigate} >
-            <View style={styles.mealContainer}>
-                <Text>{props.title}</Text>
-            </View>
-        </TouchableOpacity>
+        <View style={styles.mealItem}>
+            <TouchableOpacity style={{flex: 1}} onPress={handleNavigate} >
+                <View style={styles.mealContainer}>
+                    <Text style={styles.mealTitle}>{props.title}</Text>
+                </View>
+            </TouchableOpacity>
+        </View>
     );
 };
 
