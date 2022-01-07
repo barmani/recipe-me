6c85b4db866909ae594d93dd4322d8cca537759e
Brendan Armani <brendanarmani@gmail.com>
2022-01-06 23:00:16 -0500

    Working on swipeable edit/delete component for ingredients and steps. Now accesses close method to close it after clicking

--- components/EditableTextInput.js
+++ components/EditableTextInput.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { View, Text, TouchableOpacity, StyleSheet, TextInput } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Swipeable from 'react-native-gesture-handler/Swipeable';
@@ -33,32 +33,47 @@ const styles = StyleSheet.create({
     }
 })
 
-const RightSwipeActions = () => {
-    return (
-        <View style={styles.swipeContainer}>
-            <TouchableOpacity onPress={() => {}}>
-                <View style={styles.editContainer}>
-                    <Ionicons name="pencil" size={30} />
-                    <Text>Edit</Text>
-                </View>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => {}}>
-                <View style={styles.deleteContainer}>
-                    <Ionicons name="trash-outline" size={30} />
-                    <Text>Delete</Text>
-                </View>
-            </TouchableOpacity>
-        </View>
-    )
-}
 
 const EditableTextInput = (props) => {
+
+    const RightSwipeActions = () => {
+        return (
+            <View style={styles.swipeContainer}>
+                <TouchableOpacity onPress={editPressed}>
+                    <View style={styles.editContainer}>
+                        <Ionicons name="pencil" size={30} />
+                        <Text>Edit</Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => {}}>
+                    <View style={styles.deleteContainer}>
+                        <Ionicons name="trash-outline" size={30} />
+                        <Text>Delete</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
     const [isEditing, setIsEditing] = useState(false);
+    const swipeRef = useRef();
+
+    const closeSwipeable = () => {
+        swipeRef?.current?.close()
+    }
+
+    const editPressed = () => {
+        closeSwipeable()
+    }
 
     return (
+        
         <Swipeable
+            ref={swipeRef}
             renderRightActions={RightSwipeActions}
             onSwipeableRightOpen={() => {}}
+            rightThreshold={50}
+            overshootFriction={5}
         >
             <View style={styles.container}>
                 <Text>{props.text}</Text>
